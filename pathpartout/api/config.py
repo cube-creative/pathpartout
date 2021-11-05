1ceb7ca8f3551099573eb0361a2eaa196ea99087
a.libert <a.libert@cube-creative.com>
2021-11-05 17:47:39 +0100

    doc(api): add google docstring to API files.

--- pathpartout/api/config.py
+++ pathpartout/api/config.py
@@ -2,4 +2,14 @@ from pathpartout.application.use_cases import path_finder
 
 
 def find_label_path(config_filepath, label_name, info):
+    """Get the filepath associate to the label name, using given config file and information
+
+    Args:
+        config_filepath (str): The filepath of the Path Partout configuration file to use.
+        label_name (str): The label name corresponding to the filepath to found in the configuration.
+        info (dict of str): Information to include in the configuration.
+
+    Returns:
+        str: filepath corresponding to the given label name.
+    """
     return path_finder.find_from_label(config_filepath, label_name, info)
