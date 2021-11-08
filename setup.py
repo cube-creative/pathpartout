cbd1a7eae33f341adf1707b14b7494cbe542ca62
a.libert <a.libert@cube-creative.com>
2021-11-08 17:14:06 +0100

    bump(setup): bump to version 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 NAME = 'pathpartout'
-VERSION = '0.0.0'
+VERSION = '0.1.0'
 DESCRIPTION = 'Path Parser'
 AUTHOR = 'Cube Creative'
 AUTHOR_EMAIL = 'development@cube-creative.com'
