9cd26719842355a184f0cd980f0f0fa1ba6601d8
a.libert <a.libert@cube-creative.com>
2021-11-23 09:25:13 +0100

    improvement(tree_path): move TreePath class to api module.

--- pathpartout/infrastructure/path_tree_presenter.py
+++ pathpartout/infrastructure/path_tree_presenter.py
@@ -1,32 +1,5 @@
 from pathpartout.application.presenter import TreePresenter
-from pathpartout.api import config
-
-
-class TreePath:
-    def __init__(self):
-        self.info = dict()
-        self._available_info = list()
-        self._available_labels = list()
-        self._config_filepath = str()
-
-    @property
-    def available_info(self):
-        return self._available_info
-
-    @property
-    def available_labels(self):
-        return self._available_labels
-
-    @property
-    def config_filepath(self):
-        return self._config_filepath
-
-    def get_label_path(self, label_name):
-        if label_name not in self._available_labels:
-            print("")
-        else:
-            info = {k: v for (k, v) in self.info.items() if k in self._available_info and v is not None}
-            return config.find_label_path(self._config_filepath, label_name, info)
+from pathpartout.api.tree_path import TreePath
 
 
 class TreePathPresenter(TreePresenter):
