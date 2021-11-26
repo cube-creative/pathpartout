211a4a6c12597302af6c5fea0ecb5aad6c895bc0
a.libert <a.libert@cube-creative.com>
2021-11-26 09:32:54 +0100

    improvement(tree_builder): replace filepath name by path (can be folder path

--- pathpartout/api/tree.py
+++ pathpartout/api/tree.py
@@ -2,22 +2,65 @@ from pathpartout.application.use_cases import tree_builder
 from pathpartout.infrastructure.path_tree_presenter import TreePathPresenter
 
 
-def get_from_label(label_name, filepath):
-    """Find config associated to the given filepath and get a TreePath object with info and labels relative to it.
+def get_from_config(config_filepath):
+    """Get a TreePath object corresponding to given config filepath with empty info and labels relative to it.
+
+        This request return a Model-Presenter TreePath, with many method to interact with the api.
+        See TreePath definition for more information.
+
+        Args:
+            config_filepath (str): The filepath of the config to use.
+
+        Returns:
+            TreePath corresponding to the given config.
+        """
+    if not config_filepath:
+        raise ValueError("Path Partout: get_from_config need config_filepath argument")
+
+    tree_path_presenter = TreePathPresenter()
+    tree_builder.build_from_config(tree_path_presenter, config_filepath.lower())
+    return tree_path_presenter.tree_path
+
+
+def get_from_path(path):
+    """Find config associated to the given path and get a TreePath object with empty info and labels relative to it.
+
+        This request return a Model-Presenter TreePath, with many method to interact with the api.
+        See TreePath definition for more information.
+
+        Args:
+            path (str): The path use to find the config.
+
+        Returns:
+            TreePath corresponding to the config file found.
+        """
+    if not path:
+        raise ValueError("Path Partout: get_from_path need path argument")
+
+    tree_path_presenter = TreePathPresenter()
+    tree_builder.build_from_path(tree_path_presenter, path.lower())
+    return tree_path_presenter.tree_path
+
+
+def get_from_label(label_name, path):
+    """Find config associated to the given path and get a TreePath object with info and labels relative to it.
 
     This request return a Model-Presenter TreePath, with many method to interact with the api.
     See TreePath definition for more information.
 
     Args:
         label_name (str): The label of the config used to define tree.
-        filepath (str): The filepath corresponding to the given label.
+        path (str): The path corresponding to the given label.
 
     Returns:
         TreePath corresponding to the config file found and the given information.
 
     """
+    if not path:
+        raise ValueError("Path Partout: get_from_label need path argument")
+
     tree_path_presenter = TreePathPresenter()
-    tree_builder.build_from_label(tree_path_presenter, label_name.lower(), filepath.lower())
+    tree_builder.build_from_label(tree_path_presenter, label_name.lower(), path.lower())
     return tree_path_presenter.tree_path
 
 
--- pathpartout/application/use_cases/tree_builder.py
+++ pathpartout/application/use_cases/tree_builder.py
@@ -2,19 +2,36 @@ from pathpartout.application.entities import TreeArchitecture, ConceptualPath
 from pathpartout.application.use_cases import config_path_finder, config_reader
 
 
-def build_from_label(tree_presenter, label, filepath):
-    config_filepath = config_path_finder.find_from_filepath(filepath)
-    if not config_filepath:
-        raise ValueError("Given filepath doesn't have associate config file.")
+def build_from_path(tree_presenter, path):
+    config_filepath = config_path_finder.find_from_path(path)
+    build_from_config(tree_presenter, config_filepath)
+
+
+def build_from_label(tree_presenter, label, path):
+    config_filepath = config_path_finder.find_from_path(path)
     tree_presenter.set_config_path(config_filepath)
 
     config = config_reader.read_from_filepath(config_filepath)
     tree_architecture = TreeArchitecture.build_from_config(config)
     labels_paths = tree_architecture.get_all_path_labels()
     if not labels_paths.get(label):
-        raise ValueError(f"Given filepath doesn't have {label} label in its config file : {config_filepath}.")
+        raise ValueError(f"Path Partout: Given filepath doesn't have {label} label in its config file : {config_filepath}.")
     tree_presenter.set_labels(list(labels_paths.keys()))
 
     info = ConceptualPath.get_all_empty_info(labels_paths.values())
-    info.update(labels_paths.get(label).extract(filepath))
+    info.update(labels_paths.get(label).extract(path))
+    tree_presenter.set_info(info)
+
+
+def build_from_config(tree_presenter, config_filepath):
+    if not config_path_finder.is_valid_config_file(config_filepath):
+        raise ValueError("Path Partout: Given filepath is not valid config filepath")
+    tree_presenter.set_config_path(config_filepath)
+
+    config = config_reader.read_from_filepath(config_filepath)
+    tree_architecture = TreeArchitecture.build_from_config(config)
+    labels_paths = tree_architecture.get_all_path_labels()
+    tree_presenter.set_labels(list(labels_paths.keys()))
+
+    info = ConceptualPath.get_all_empty_info(labels_paths.values())
     tree_presenter.set_info(info)
