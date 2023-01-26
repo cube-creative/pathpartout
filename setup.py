b000bc7684da24cfa50ea962aff290b1fe155bb3
rpatalowski <rpatalowski@xilam.com>
2023-01-26 17:17:56 +0100

    [BUMP] Bump version 1.2.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 NAME = 'pathpartout'
-VERSION = '1.2.2'
+VERSION = '1.2.3'
 DESCRIPTION = 'Path Parser'
 AUTHOR = 'Cube Creative'
 AUTHOR_EMAIL = 'development@cube-creative.com'
