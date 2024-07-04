8dc38ef0ae4fde583e272c4a3e07a5fb79aaffd7
s.martinez <smartinez@xilam.com>
2024-07-04 17:55:06 +0200

    feat: add test

--- tests/tree_test.py
+++ tests/tree_test.py
@@ -34,6 +34,12 @@ class TestTree(TestCase):
         generated_shot_working_file = tree.get_label_path('shot_working_file')
         self.assertEqual(self.initial_shot_working_file.lower(), generated_shot_working_file.lower())
 
+    def test_tree_fill_with_agregate(self):
+        tree = pathpartout.tree.get_from_config(f'{SAMPLE_PROJECT_ROOT}/sample.conf')
+        tree.info["project_name"] = SAMPLE_PROJECT_NAME
+        tree.fill_with_aggregate("episode_full_name", 's01e018_bearselandgretel')
+        folder_path = tree.get_label_path('episode_library_folder')
 
+        self.assertEqual(f"{SAMPLE_PROJECT_ROOT}/{SAMPLE_PROJECT_NAME}/episodes/s01e018_bearselandgretel".lower(), folder_path.lower())
 if __name__ == '__main__':
     main()
